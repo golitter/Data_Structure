3a4b1778ca142f8f9622fa827369bcd9c0e3b0ee
golitter <1981971524@qq.com>
2024-03-07 17:51:35 +0800

    中序线索二叉树的逆向 + 代码注释

--- tree/cued_binary_tree.cpp
+++ tree/cued_binary_tree.cpp
@@ -73,7 +73,13 @@ ThreadNode* FistNode(ThreadNode *p) {
     return p;
 }
 
+// ***********************  后继    **************************************
+
 // 在中序线索二叉树中找到结点p的后继结点
+    /**
+     * 1. 如果p的rtag == 1，那么p的后继结点是p->rchild
+     * 2. 如果p的rtag == 0，那么p的后继结点是以p->rchild为根的子树中第一个被中序遍历的结点
+    */
 ThreadNode* NextNode(ThreadNode *p) {
     if(p->rtag == 0) {
         return FistNode(p->rchild);
@@ -82,9 +88,42 @@ ThreadNode* NextNode(ThreadNode *p) {
     }
 }
 
-// 中序遍历线索二叉树
+// 中序正向遍历线索二叉树
 void InOrder(ThreadTree T) {
     for(ThreadNode *p = FistNode(T); p != NULL; p = NextNode(p)) {
+        cout<<p->data<<" ";
+    }
+    cout<<endl;
+}
+
+// ***********************  前驱    **************************************
+
+// 在中序线索二叉树中找到结点p的前驱结点
+    /**
+     * 1. 如果p的ltag == 1，那么p的前驱结点是p->lchild
+     * 2. 如果p的ltag == 0，那么p的前驱结点是以p->lchild为根的子树中最后一个被中序遍历的结点
+    */
+
+// 最后一个被中序遍历的结点
+ThreadNode* LastNode(ThreadNode *p) {
+    while(p->rtag == 0) {
+        p = p->rchild;
+    }
+    return p;
+}
+
+// 在中序线索二叉树中找到结点p的前驱结点
+ThreadNode* PreNode(ThreadNode *p) {
+    if(p->ltag == 0) {
+        return LastNode(p->lchild);
+    } else {
+        return p->lchild;
+    }
+}
+
+// 中序逆向遍历线索二叉树
+void InOrderReverse(ThreadTree T) {
+    for(ThreadNode *p = LastNode(T); p != NULL; p = PreNode(p)) {
         cout<<p->data<<" ";
     }
     cout<<endl;
@@ -93,7 +132,11 @@ void solve() {
     ThreadTree T;
     testInitTree(T);
     creatInThread(T);
+
+    // 中序正向遍历
     InOrder(T);
+    // 中序逆向遍历
+    InOrderReverse(T);
 
 }
 int main()
